3d9fca4977b2eba851350dfbfd7ae56a93db12d5
yavlash-liza <yavlashliza@gmail.com>
2022-03-06 12:35:43 +0300

    refactoring repositories

--- src/main/java/by/library/yavlash/repository/impl/GenreRepositoryImpl.java
+++ src/main/java/by/library/yavlash/repository/impl/GenreRepositoryImpl.java
@@ -1,77 +1,79 @@
-//package by.library.yavlash.repository.impl;
-//
-//import by.library.yavlash.entity.Genre;
-//import by.library.yavlash.repository.GenreRepository;
-//
-//import javax.sql.DataSource;
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//
-//public class GenreRepositoryImpl extends AbstractRepositoryImpl<Genre> implements GenreRepository {
-//    private static final String ID_COLUMN = "id";
-//    private static final String GENRE_NAME_COLUMN = "genre_name";
-//
-//    private static final String SELECT_BY_ID_QUERY = "SELECT * FROM genres WHERE id=?";
-//    private static final String SELECT_ALL_QUERY = "SELECT * FROM genres";
-//    private static final String INSERT_QUERY = "INSERT INTO genres (genre_name) VALUES (?)";
-//    private static final String UPDATE_QUERY = "UPDATE genres SET genre_name=? WHERE id=?";
-//    private static final String DELETE_QUERY = "DELETE FROM genres WHERE id=?";
-//
-//    private static final String DELETE_BOOK_GENRE_LINKS_QUERY = "DELETE FROM book_genre_links WHERE genre_id=?";
-//
-//    public GenreRepositoryImpl(DataSource dataSource) {
-//        super(dataSource);
-//    }
-//
-//    @Override
-//    protected String getSelectByIdQuery() {
-//        return SELECT_BY_ID_QUERY;
-//    }
-//
-//    @Override
-//    protected String getSelectAllQuery() {
-//        return SELECT_ALL_QUERY;
-//    }
-//
-//    @Override
-//    protected String getInsertQuery() {
-//        return INSERT_QUERY;
-//    }
-//
-//    @Override
-//    protected String getUpdateQuery() {
-//        return UPDATE_QUERY;
-//    }
-//
-//    @Override
-//    protected String getDeleteQuery() {
-//        return DELETE_QUERY;
-//    }
-//
-//    @Override
-//    protected Genre construct(ResultSet resultSet) throws SQLException {
-//        return Genre.builder()
-//                .id(resultSet.getLong(ID_COLUMN))
-//                .genreName(resultSet.getString(GENRE_NAME_COLUMN))
-//                .build();
-//    }
-//
-//    @Override
-//    protected void settingPreparedStatement(PreparedStatement preparedStatement, Genre genre) throws SQLException {
-//        preparedStatement.setString(1, genre.getGenreName());
-//    }
-//
-//    @Override
-//    protected void deleteLinks(Connection connection, Long genreId) throws SQLException {
-//        deleteGenreLinks(connection, genreId);
-//    }
-//
-//    protected void deleteGenreLinks(Connection connection, Long id) throws SQLException {
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(GenreRepositoryImpl.DELETE_BOOK_GENRE_LINKS_QUERY)) {
-//            preparedStatement.setLong(1, id);
-//            preparedStatement.executeUpdate();
-//        }
-//    }
-//}
+package by.library.yavlash.repository.impl;
+
+import by.library.yavlash.entity.Genre;
+import by.library.yavlash.exception.RepositoryException;
+import by.library.yavlash.repository.GenreRepository;
+import by.library.yavlash.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class GenreRepositoryImpl implements GenreRepository {
+    private static final String ID_COLUMN = "id";
+    private static final String GENRE_NAME_COLUMN = "genreName";
+
+    private static final String SELECT_ALL_QUERY = "from Genre";
+    private static final String UPDATE_QUERY = "update Genre set genreName=:genreName where id=:id";
+
+    @Override
+    public Genre findById(Long id) throws RepositoryException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Genre.class, id);
+        }
+    }
+
+    @Override
+    public List<Genre> findAll() throws RepositoryException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery(SELECT_ALL_QUERY, Genre.class).list();
+        }
+    }
+
+    @Override
+    public boolean add(Genre genre) throws RepositoryException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            session.save(genre);
+            return true;
+        }
+    }
+
+    @Override
+    public boolean update(Genre genre) throws RepositoryException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            try {
+                session.getTransaction().begin();
+                Query query = session.createQuery(UPDATE_QUERY);
+                constructQuery(query, genre);
+                query.executeUpdate();
+                session.getTransaction().commit();
+                return true;
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+                throw new RepositoryException("Genre was not updated[" + ex.getMessage() + "]");
+            }
+        }
+    }
+
+    @Override
+    public boolean delete(Long id) throws RepositoryException {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            try {
+                session.getTransaction().begin();
+                Genre genre = session.get(Genre.class, id);
+                genre.getBooks().forEach(book -> book.getGenres().remove(genre));
+                session.delete(genre);
+                session.getTransaction().commit();
+                return true;
+            } catch (Exception ex) {
+                session.getTransaction().rollback();
+                throw new RepositoryException("Genre was not deleted[" + ex.getMessage() + "]");
+            }
+        }
+    }
+
+    private void constructQuery(Query query, Genre genre){
+        query.setParameter(GENRE_NAME_COLUMN, genre.getGenreName())
+                .setParameter(ID_COLUMN, genre.getId());
+    }
+}
