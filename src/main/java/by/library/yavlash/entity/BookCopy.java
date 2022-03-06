47c65fd8d806cbc6b87cf35060e25362e891fa87
yavlash-liza <yavlashliza@gmail.com>
2022-03-06 15:23:08 +0300

    refactoring entities

--- src/main/java/by/library/yavlash/entity/BookCopy.java
+++ src/main/java/by/library/yavlash/entity/BookCopy.java
@@ -7,7 +7,17 @@ import lombok.experimental.SuperBuilder;
 import lombok.AllArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -15,11 +25,34 @@ import java.time.LocalDate;
 @ToString(callSuper = true)
 @SuperBuilder
 @AllArgsConstructor
+@Entity
+@Table(name = "book_copies")
 public class BookCopy extends BaseEntity {
-    private Long id;
+    @Column(name = "book_copy_status")
     private String status;
+
+    @Column(name = "registration_date")
     private LocalDate registrationDate;
+
+    @Column(name = "image_path")
     private String imagePath;
+
+    @Column(name = "price_per_day")
     private int pricePerDay;
-    private Long bookId;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(mappedBy = "bookCopies", fetch = FetchType.LAZY)
+    private Set<Order> orders = new HashSet<>();
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "bookCopy", fetch = FetchType.LAZY)
+    private Set<BookDamage> bookDamages = new HashSet<>();
 }
