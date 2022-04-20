27da684bd34382e933d399bd219409edeccbb7c3
yavlash-liza <yavlashliza@gmail.com>
2022-04-20 11:07:41 +0300

    add configurations to tests

---
+++ src/test/java/by/library/yavlash/config/TestServiceConfiguration.java
@@ -0,0 +1,9 @@
+package by.library.yavlash.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("by.library.yavlash.service")
+public class TestServiceConfiguration {
+}
