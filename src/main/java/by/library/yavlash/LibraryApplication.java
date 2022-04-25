b4fea7252ce10a9389898b00adb3952fe36508f1
yavlash-liza <yavlashliza@gmail.com>
2022-04-25 22:20:06 +0300

    refactoring configurations

---
+++ src/main/java/by/library/yavlash/LibraryApplication.java
@@ -0,0 +1,11 @@
+package by.library.yavlash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+}
